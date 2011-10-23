afc4a7c8654ffc8889d0d4d1d6e50d386b587db4
Miguel Perez <miguel250@gmail.com>
2011-10-22 23:42:28 -0400

    Setup mail chimp class and http client

---
+++ Services/HttpClient.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the MZ\MailChimpBundle
+ *
+ * (c) Miguel Perez <miguel@mlpz.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace MZ\MailChimpBundle\Services;
+
+class HttpClient
+{
+    protected $url = "http://us2.api.mailchimp.com/1.3/";
+    protected $apiKey;
+
+    /**
+     * Send API request to mailchimp
+     *
+     * @return array
+     */
+    protected function makeRequest($type, $payload)
+    {
+        $payload['apikey'] = $this->apiKey;
+        $data = json_encode($payload);
+        $url = $this->url . '?method=' . $type;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, "MZMailChimpBundle");
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, urlencode($data));
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $data = json_decode($result);
+
+        return $data;
+    }
+
+}
---
+++ Services/MailChimp.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the MZ\MailChimpBundle
+ *
+ * (c) Miguel Perez <miguel@mlpz.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace MZ\MailChimpBundle\Services;
+
+class MailChimp
+{
+
+    private $apiKey;
+    private $listId;
+
+    /**
+     * Initializes MailChimp
+     *
+     * @param string $apiKey
+     * @param string $listId
+     */
+    public function __construct($apiKey, $listId)
+    {
+        $this->apiKey = $apiKey;
+        $this->listId = $listId;
+
+        if (!function_exists('curl_init')) {
+            throw new Exception('This bundle needs the cURL PHP extension.');
+        }
+    }
+
+    /**
+     * Set mailing list id
+     *
+     * @param string $listId
+     */
+    public function setListID($listId)
+    {
+        $this->listId = $listId;
+    }
+}
