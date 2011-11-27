38aa738ad89f66cc60750b02934551b7684b07ed
Miguel Perez <miguel250@gmail.com>
2011-11-26 21:07:33 -0500

    Add Support for xml config

--- DependencyInjection/MZMailChimpExtension.php
+++ DependencyInjection/MZMailChimpExtension.php
@@ -40,4 +40,32 @@ class MZMailChimpExtension extends Extension
             }
         }
     }
+    
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getXsdValidationBasePath()
+    {
+    	return __DIR__ . '/../Resources/config/schema';
+    }
+    
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getNamespace()
+    {
+    	return 'http://symfony.com/schema/dic/mz_mailchimp';
+    }
+    
+    /**
+     * @codeCoverageIgnore
+     */
+    protected function loadDefaults($container)
+    {
+    	$loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+    
+    	foreach ($this->resources as $resource) {
+    		$loader->load($resource);
+    	}
+    }
 }
