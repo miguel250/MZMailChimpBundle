306ecc586d4c39003459bba209ffd2a88a12f833
Miguel Perez <miguel250@gmail.com>
2011-11-26 19:48:17 -0500

    Add missing license header

--- Services/Methods/MCCampaign.php
+++ Services/Methods/MCCampaign.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the MZ\MailChimpBundle
+*
+* (c) Miguel Perez <miguel@mlpz.com>
+*
+* This source file is subject to the MIT license that is bundled
+* with this source code in the file LICENSE.
+*/
+
 namespace MZ\MailChimpBundle\Services\Methods;
 
 use MZ\MailChimpBundle\Services\HttpClient;
